f6334a9ae8995ed7787efb47f22ebb68adb450df
Francisco Flores <fflores.ic.ipn@hotmail.com>
2020-08-14 16:12:39 -0500

    AutoPause de JS a TS

--- assets/plugins/AutoPause.ts
+++ assets/plugins/AutoPause.ts
@@ -1,4 +1,8 @@
+import MediaPlayer from "../MediaPlayer";
+
 export default class AutoPause {
+    private threshold: number;
+    player: MediaPlayer;
 
     constructor() {
         this.threshold = 0.25;
@@ -18,9 +22,9 @@ export default class AutoPause {
         document.addEventListener('visibilitychange', this.handleVisibilityChange)
     }
 
-    handleIntersection(entries) {
+    private handleIntersection(entries: IntersectionObserverEntry[]) {
         const entry = entries[0];
-
+        
         const isVisible = entry.intersectionRatio >= this.threshold;
 
         if (isVisible) {
@@ -30,7 +34,7 @@ export default class AutoPause {
         }
     }
 
-    handleVisibilityChange() {
+    private handleVisibilityChange() {
         const isVisible = document.visibilityState === 'visible';
 
         if (isVisible) {
